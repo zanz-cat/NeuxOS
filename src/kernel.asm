110459b7b5a39bd90fa0585ea1a6db7d620d16a9
lee0211 <linus_linux@163.com>
2019-12-08 11:33:54 +0800

    fly

--- src/kernel.asm
+++ src/kernel.asm
@@ -5,18 +5,50 @@ org 0100h
     jmp LABEL_BEGIN
 
 [SECTION .gdt]
-;                     Base, Limit, Attributes                      
-LABEL_GDT: Descriptor 0,    0,     0
-LABEL_DESC_CODE32: Descriptor 0, SegCode32Len - 1, DA_C + DA_32
-LABEL_DESC_VIDEO: Descriptor 0b8000h, 0ffffh,   DA_DRW
+;                                Base,         Limit,            Attributes                      
+LABEL_GDT:            Descriptor 0,            0,                0
+LABEL_DESC_NORMAL:    Descriptor 0,            0ffffh,           DA_DRW
+LABEL_DESC_CODE32:    Descriptor 0,            SegCode32Len - 1, DA_C + DA_32
+LABEL_DESC_CODE16:    Descriptor 0,            0ffffh,           DA_C
+LABEL_DESC_DATA:      Descriptor 0,            DataLen - 1,      DA_DRW
+LABEL_DESC_STACK:     Descriptor 0,            TopOfStack,       DA_DRWA+DA_32
+LABEL_DESC_TEST:      Descriptor 0500000h,     0ffffh,           DA_DRW
+LABEL_DESC_VIDEO:     Descriptor 0b8000h,      0ffffh,           DA_DRW
 
 GDTLen equ $ - LABEL_GDT
 GDTPtr dw GDTLen - 1	;GDT Limit
        dd 0		;GDT Base Address
 
 ;GDT Selectors
-SelectorCode32 equ LABEL_DESC_CODE32 - LABEL_GDT
-SelectorVideo  equ LABEL_DESC_VIDEO - LABEL_GDT
+SelectorNormal        equ LABEL_DESC_NORMAL - LABEL_GDT
+SelectorCode32        equ LABEL_DESC_CODE32 - LABEL_GDT
+SelectorCode16        equ LABEL_DESC_CODE16 - LABEL_GDT
+SelectorData          equ LABEL_DESC_DATA   - LABEL_GDT
+SelectorStack         equ LABEL_DESC_STACK  - LABEL_GDT
+SelectorTest          equ LABEL_DESC_TEST   - LABEL_GDT
+SelectorVideo         equ LABEL_DESC_VIDEO  - LABEL_GDT
+
+
+[SECTION .data1]
+ALIGN 32
+[BITS 32]
+LABEL_DATA:
+SPValueInRealMode	dw 0
+
+PMMessage:	db "In Protest Mode now. @_@", 0
+OffsetPMMessage	equ PMMessage - $$
+
+StrTest:	db "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0
+OffsetStrTest	equ StrTest - $$
+
+DataLen		equ $ - LABEL_DATA
+
+
+[SECTION .gs]
+ALIGN 32
+[BITS 32]
+LABEL_STACK: times 512 db 0
+TopOfStack equ $ - LABEL_STACK - 1
 
 
 [SECTION .s16]
@@ -28,6 +60,19 @@ LABEL_BEGIN:
     mov es, ax
     mov sp, 0100h
 
+	mov [LABEL_GO_BACK_TO_REAL + 3], ax
+	mov [SPValueInRealMode], sp
+
+	; initialize 16bit code segment descriptor
+	mov ax, cs
+	movzx eax, ax
+	shl eax, 4
+	add eax, LABEL_SEG_CODE16
+	mov word [LABEL_DESC_CODE16 + 2], ax
+	shr eax, 16
+	mov byte [LABEL_DESC_CODE16 + 4], al
+	mov byte [LABEL_DESC_CODE16 + 7], ah
+
     ; initialize 32bit code segment descriptor
     xor eax, eax
     mov ax, cs
@@ -37,6 +82,26 @@ LABEL_BEGIN:
     shr eax, 16
     mov byte [LABEL_DESC_CODE32 + 4], al
     mov byte [LABEL_DESC_CODE32 + 7], ah
+
+	; initialize data segment descriptor
+	xor eax, eax
+	mov ax, ds
+	shl eax, 4
+	add eax, LABEL_DATA
+	mov word [LABEL_DESC_DATA + 2], ax
+	shr eax, 16
+	mov byte [LABEL_DESC_DATA + 4], al
+	mov byte [LABEL_DESC_DATA + 7], ah
+
+	; initialize stack segment descriptor
+	xor eax, eax
+	mov ax, ds
+	shl eax, 4
+	add eax, LABLE_STACK
+	mov word [LABEL_DESC_STACK + 2], ax
+	shr eax, 16
+	mov byte [LABEL_DESC_STACK + 4], al
+	mov byte [LABEL_DESC_STACK + 7], ah
 
     ; initialize GDT Pointer
     xor eax, eax
