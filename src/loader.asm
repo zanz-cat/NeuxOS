74db7b74fa8df4b9454d4278fb84e4d70e609744
lee0211 <linus_linux@163.com>
2020-03-22 21:40:50 +0800

    oo

--- src/loader.asm
+++ src/loader.asm
@@ -1,13 +1,10 @@
 org 0x100
-; xchg bx, bx
 jmp LABEL_START
 
-%include "common.inc"
+%include "loader.inc"
 
 LOADING_MESSAGE      db  'Loading...'
 LOADING_MESSAGE_LEN  equ $ - LOADING_MESSAGE
-
-; times 1024 db 0
 
 LABEL_START:
     mov ax, cs
