47eb6c6ab50a5b467b28c2641e0420691c43323d
liwei <linus_linux@163.com>
2020-05-01 19:45:41 +0800

    c

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,12 +1,33 @@
-SELECTOR_KERNEL_CS  equ 8
+; SELECTOR_KERNEL_CS  equ 8
+
+; extern cstart
+; extern gdt_ptr
+
+; [section .bss]
+; StackSpace  resb    2 * 1024
+; StackTop: 
+
+; [section .text]
+; global _start
+
+; _start:
+;     mov ah, 0fh
+;     mov al, 'K'
+;     mov [gs:((80 * 1 + 39) * 2)], ax
+
+;     mov esp, StackTop
+;     sgdt [gdt_ptr]
+;     call cstart
+;     lgdt [gdt_ptr]
+
+;     jmp SELECTOR_KERNEL_CS:csinit
+
+; csinit:
+;     push 0
+;     popfd
+;     hlt
 
 extern cstart
-extern gdt_ptr
-
-[section .bss]
-StackSpace  resb    2 * 1024
-StackTop: 
-
 [section .text]
 global _start
 
@@ -14,15 +35,4 @@ _start:
     mov ah, 0fh
     mov al, 'K'
     mov [gs:((80 * 1 + 39) * 2)], ax
-
-    mov esp, StackTop
-    sgdt [gdt_ptr]
     call cstart
-    lgdt [gdt_ptr]
-
-    jmp SELECTOR_KERNEL_CS:csinit
-
-csinit:
-    push 0
-    popfd
-    hlt
