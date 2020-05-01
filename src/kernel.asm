2fda0c9fdfecbe479e5bf8188a9fbb285faa11b6
Will Lee <linus_linux@163.com>
2020-05-01 15:26:21 +0800

    1

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,10 +1,28 @@
-[SECTION .bss]
+SELECTOR_KERNEL_CS  equ 8
 
-[SECTION .text]
+extern cstart
+extern gdt_ptr
+
+[section .bss]
+StackSpace  resb    2 * 1024
+StackTop: 
+
+[section .text]
 global _start
 
 _start:
     mov ah, 0fh
     mov al, 'K'
     mov [gs:((80 * 1 + 39) * 2)], ax
-    jmp $
+
+    mov esp, StackTop
+    sgdt [gdt_ptr]
+    call cstart
+    lgdt [gdt_ptr]
+
+    jmp SELECTOR_KERNEL_CS:csinit
+
+csinit:
+    push 0
+    popfd
+    hlt
